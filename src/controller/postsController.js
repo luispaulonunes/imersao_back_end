97460c67a208902f0ab302c6749d18bfe6cb70cc
luispaulonunes <qtaltchepilchas@gmail.com>
2025-01-07 15:52:20 -0300

    quarta aula

--- src/controller/postsController.js
+++ src/controller/postsController.js
@@ -1,8 +1,33 @@
-import getTodosPosts from "../models/postsModel.js";
+import {getTodosPosts, criarPost, uploadImagem} from "../models/postsModel.js";
 
 export async  function listarPosts(req, res) {
     // Chama a função para obter os posts
     const posts = await  getTodosPosts();
     // Envia os posts como resposta em formato JSON com status 200 (sucesso)
-    res.status(200).json(posts);
+    res.status(200).json(posts);    
+  }
+
+  export async function postarNovoPost(req, res) {
+    const novoPost = req.body;
+    try {
+      const postCriado = await criarPost (novoPost);
+      res.status(200).json(postCriado);
+    }
+    catch(erro) {
+      console.error(erro.message);
+      res.status(500).json({"Erro": "Falha na requisicao"});
+    }
+    
+  }
+
+  export async function uploadImagem(req, res) {
+    const novoPost = req.body;
+    try {
+      const postCriado = await criarPost (novoPost);
+      res.status(200).json(postCriado);
+    }
+    catch(erro) {
+      console.error(erro.message);
+      res.status(500).json({"Erro": "Falha na requisicao"});
+    }
   }
